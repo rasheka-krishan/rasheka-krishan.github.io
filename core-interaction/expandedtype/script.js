f13b43ca30a270c6bc0ceec31ddd8224945cc0f7
RashekaKrishan <krisr673@newschool.edu>
2018-03-29 23:04:49 -0400

    user input

--- core-interaction/expandedtype/script.js
+++ core-interaction/expandedtype/script.js
@@ -1,326 +1,106 @@
 function userWord() {
-    var word = document.getElementById("myText").value;
+    var word = document.getElementById("input").value;
     var length = word.length;
-    document.getElementById("demo").innerHTML = word;
+
+    var spanny_word = '<span>' + word.split('').join('</span><span>') + '</span>';
+
+    document.getElementById("word").innerHTML = spanny_word;
 }
 
 
 function sadFunction() {
   //remove
-  var element = document.getElementById("firstletter");
+  var element = document.getElementById("word");
   element.classList.remove("angry");
 
-  var element = document.getElementById("secondletter");
-  element.classList.remove("angry");
+  var element = document.getElementById("word");
+  element.classList.remove("loveletter");
 
-  var element = document.getElementById("thirdletter");
-  element.classList.remove("angry");
-
-  var element = document.getElementById("thirdtolastletter");
-  element.classList.remove("angry");
-
-  var element = document.getElementById("secondtolastletter");
-  element.classList.remove("angry");
-
-  var element = document.getElementById("lastletter");
-  element.classList.remove("angry");
-
-  var element = document.getElementById("firstletter");
-  element.classList.remove("lovefirstletter");
-
-  var element = document.getElementById("secondletter");
-  element.classList.remove("lovesecondletter");
-
-  var element = document.getElementById("thirdletter");
-  element.classList.remove("lovethirdletter");
-
-  var element = document.getElementById("thirdtolastletter");
-  element.classList.remove("lovethirdtolastletter");
-
-  var element = document.getElementById("secondtolastletter");
-  element.classList.remove("lovesecondtolastletter");
-
-  var element = document.getElementById("lastletter");
-  element.classList.remove("lovelastletter");
-
-  var element = document.getElementById("firstletter");
+  var element = document.getElementById("word");
   element.classList.remove("excited");
-
-  var element = document.getElementById("secondletter");
-  element.classList.remove("excited");
-
-  var element = document.getElementById("thirdletter");
-  element.classList.remove("excited");
-
-  var element = document.getElementById("thirdtolastletter");
-  element.classList.remove("excited");
-
-  var element = document.getElementById("secondtolastletter");
-  element.classList.remove("excited");
-
-  var element = document.getElementById("lastletter");
-  element.classList.remove("excited");
-
 
   //toggle
-  var element = document.getElementById("firstletter");
-  element.classList.toggle("sadfirstletter");
 
-  var element = document.getElementById("secondletter");
-  element.classList.toggle("sadsecondletter");
+  var spansArray = document.querySelectorAll('span');
+  spansArray[spansArray.length-3].classList.toggle('sadthirdtolastletter');
 
-  var element = document.getElementById("thirdletter");
-  element.classList.toggle("sadthirdletter");
+  var spansArray = document.querySelectorAll('span');
+  spansArray[spansArray.length-2].classList.toggle('sadsecondtolastletter');
 
-  var element = document.getElementById("lastletter");
-  element.classList.toggle("sadlastletter");
+  var spansArray = document.querySelectorAll('span');
+  spansArray[spansArray.length-1].classList.toggle('sadlastletter');
 
-  var element = document.getElementById("secondtolastletter");
-  element.classList.toggle("sadsecondtolastletter");
-
-  var element = document.getElementById("thirdtolastletter");
-  element.classList.toggle("sadthirdtolastletter");
-
-
-  // var element = document.getElementById("thirdletter");
-  // element.classList.toggle("sadthirdletter");
-  //
-  // var element = document.getElementById("secondletter");
-  // element.classList.toggle("sadsecondletter");
 }
 
 function excitedFunction() {
   //remove
-  var element = document.getElementById("firstletter");
-  element.classList.remove("sadfirstletter");
 
-  var element = document.getElementById("secondletter");
-  element.classList.remove("sadsecondletter");
-
-  var element = document.getElementById("thirdletter");
-  element.classList.remove("sadthirdletter");
-
-  var element = document.getElementById("lastletter");
-  element.classList.remove("sadlastletter");
-
-  var element = document.getElementById("secondtolastletter");
-  element.classList.remove("sadsecondtolastletter");
-
-  var element = document.getElementById("thirdtolastletter");
-  element.classList.remove("sadthirdtolastletter");
-
-  var element = document.getElementById("firstletter");
+  var element = document.getElementById("word");
   element.classList.remove("angry");
 
-  var element = document.getElementById("secondletter");
-  element.classList.remove("angry");
+  var element = document.getElementById("word");
+  element.classList.remove("loveletter");
 
-  var element = document.getElementById("thirdletter");
-  element.classList.remove("angry");
+  var spansArray = document.querySelectorAll('span');
+  spansArray[spansArray.length-3].classList.remove('sadthirdtolastletter');
 
-  var element = document.getElementById("thirdtolastletter");
-  element.classList.remove("angry");
+  var spansArray = document.querySelectorAll('span');
+  spansArray[spansArray.length-2].classList.remove('sadsecondtolastletter');
 
-  var element = document.getElementById("secondtolastletter");
-  element.classList.remove("angry");
-
-  var element = document.getElementById("lastletter");
-  element.classList.remove("angry");
-
-  var element = document.getElementById("firstletter");
-  element.classList.remove("lovefirstletter");
-
-  var element = document.getElementById("secondletter");
-  element.classList.remove("lovesecondletter");
-
-  var element = document.getElementById("thirdletter");
-  element.classList.remove("lovethirdletter");
-
-  var element = document.getElementById("thirdtolastletter");
-  element.classList.remove("lovethirdtolastletter");
-
-  var element = document.getElementById("secondtolastletter");
-  element.classList.remove("lovesecondtolastletter");
-
-  var element = document.getElementById("lastletter");
-  element.classList.remove("lovelastletter");
+  var spansArray = document.querySelectorAll('span');
+  spansArray[spansArray.length-1].classList.remove('sadlastletter');
 
   //toggle
-  var element = document.getElementById("firstletter");
+  var element = document.getElementById("word");
   element.classList.toggle("excited");
-
-  var element = document.getElementById("secondletter");
-  element.classList.toggle("excited");
-
-  var element = document.getElementById("thirdletter");
-  element.classList.toggle("excited");
-
-  var element = document.getElementById("thirdtolastletter");
-  element.classList.toggle("excited");
-
-  var element = document.getElementById("secondtolastletter");
-  element.classList.toggle("excited");
-
-  var element = document.getElementById("lastletter");
-  element.classList.toggle("excited");
-
 }
 
 function angryFunction(){
 
   //remove
-  var element = document.getElementById("firstletter");
-  element.classList.remove("sadfirstletter");
 
-  var element = document.getElementById("secondletter");
-  element.classList.remove("sadsecondletter");
+  var element = document.getElementById("word");
+  element.classList.remove("loveletter");
 
-  var element = document.getElementById("thirdletter");
-  element.classList.remove("sadthirdletter");
-
-  var element = document.getElementById("lastletter");
-  element.classList.remove("sadlastletter");
-
-  var element = document.getElementById("secondtolastletter");
-  element.classList.remove("sadsecondtolastletter");
-
-  var element = document.getElementById("thirdtolastletter");
-  element.classList.remove("sadthirdtolastletter");
-
-  var element = document.getElementById("firstletter");
-  element.classList.remove("lovefirstletter");
-
-  var element = document.getElementById("secondletter");
-  element.classList.remove("lovesecondletter");
-
-  var element = document.getElementById("thirdletter");
-  element.classList.remove("lovethirdletter");
-
-  var element = document.getElementById("thirdtolastletter");
-  element.classList.remove("lovethirdtolastletter");
-
-  var element = document.getElementById("secondtolastletter");
-  element.classList.remove("lovesecondtolastletter");
-
-  var element = document.getElementById("lastletter");
-  element.classList.remove("lovelastletter");
-
-  var element = document.getElementById("firstletter");
+  var element = document.getElementById("word");
   element.classList.remove("excited");
 
-  var element = document.getElementById("secondletter");
-  element.classList.remove("excited");
+  var spansArray = document.querySelectorAll('span');
+  spansArray[spansArray.length-3].classList.remove('sadthirdtolastletter');
 
-  var element = document.getElementById("thirdletter");
-  element.classList.remove("excited");
+  var spansArray = document.querySelectorAll('span');
+  spansArray[spansArray.length-2].classList.remove('sadsecondtolastletter');
 
-  var element = document.getElementById("thirdtolastletter");
-  element.classList.remove("excited");
-
-  var element = document.getElementById("secondtolastletter");
-  element.classList.remove("excited");
-
-  var element = document.getElementById("lastletter");
-  element.classList.remove("excited");
-
+  var spansArray = document.querySelectorAll('span');
+  spansArray[spansArray.length-1].classList.remove('sadlastletter');
 
   //toggle
 
-  var element = document.getElementById("firstletter");
-  element.classList.toggle("angry");
-
-  var element = document.getElementById("secondletter");
-  element.classList.toggle("angry");
-
-  var element = document.getElementById("thirdletter");
-  element.classList.toggle("angry");
-
-  var element = document.getElementById("thirdtolastletter");
-  element.classList.toggle("angry");
-
-  var element = document.getElementById("secondtolastletter");
-  element.classList.toggle("angry");
-
-  var element = document.getElementById("lastletter");
+  var element = document.getElementById("word");
   element.classList.toggle("angry");
 }
 
 function loveFunction(){
 
   //remove
-  var element = document.getElementById("firstletter");
-  element.classList.remove("sadfirstletter");
 
-  var element = document.getElementById("secondletter");
-  element.classList.remove("sadsecondletter");
-
-  var element = document.getElementById("thirdletter");
-  element.classList.remove("sadthirdletter");
-
-  var element = document.getElementById("lastletter");
-  element.classList.remove("sadlastletter");
-
-  var element = document.getElementById("secondtolastletter");
-  element.classList.remove("sadsecondtolastletter");
-
-  var element = document.getElementById("thirdtolastletter");
-  element.classList.remove("sadthirdtolastletter");
-
-  var element = document.getElementById("firstletter");
+  var element = document.getElementById("word");
   element.classList.remove("excited");
 
-  var element = document.getElementById("secondletter");
-  element.classList.remove("excited");
-
-  var element = document.getElementById("thirdletter");
-  element.classList.remove("excited");
-
-  var element = document.getElementById("thirdtolastletter");
-  element.classList.remove("excited");
-
-  var element = document.getElementById("secondtolastletter");
-  element.classList.remove("excited");
-
-  var element = document.getElementById("lastletter");
-  element.classList.remove("excited");
-
-  var element = document.getElementById("firstletter");
+  var element = document.getElementById("word");
   element.classList.remove("angry");
 
-  var element = document.getElementById("secondletter");
-  element.classList.remove("angry");
+  var spansArray = document.querySelectorAll('span');
+  spansArray[spansArray.length-3].classList.remove('sadthirdtolastletter');
 
-  var element = document.getElementById("thirdletter");
-  element.classList.remove("angry");
+  var spansArray = document.querySelectorAll('span');
+  spansArray[spansArray.length-2].classList.remove('sadsecondtolastletter');
 
-  var element = document.getElementById("thirdtolastletter");
-  element.classList.remove("angry");
-
-  var element = document.getElementById("secondtolastletter");
-  element.classList.remove("angry");
-
-  var element = document.getElementById("lastletter");
-  element.classList.remove("angry");
-
+  var spansArray = document.querySelectorAll('span');
+  spansArray[spansArray.length-1].classList.remove('sadlastletter');
 
   //toggle
-  var element = document.getElementById("firstletter");
-  element.classList.toggle("lovefirstletter");
-
-  var element = document.getElementById("secondletter");
-  element.classList.toggle("lovesecondletter");
-
-  var element = document.getElementById("thirdletter");
-  element.classList.toggle("lovethirdletter");
-
-  var element = document.getElementById("thirdtolastletter");
-  element.classList.toggle("lovethirdtolastletter");
-
-  var element = document.getElementById("secondtolastletter");
-  element.classList.toggle("lovesecondtolastletter");
-
-  var element = document.getElementById("lastletter");
-  element.classList.toggle("lovelastletter");
-
+  var element = document.getElementById("word");
+  element.classList.toggle("loveletter");
 
 }
